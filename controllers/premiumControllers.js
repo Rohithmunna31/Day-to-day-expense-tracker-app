a08d5abd740f2e8c3a6a3fd5dae53b9e2e859e5f
Rohithmunna31 <rohithkasna31@gmail.com>
2024-02-29 18:50:11 +0530

    done leaderboard optimization

--- controllers/premiumControllers.js
+++ controllers/premiumControllers.js
@@ -8,29 +8,21 @@ const User = require("../models/User");
 
 exports.getshowleaderboard = async (req, res, next) => {
   try {
-    const users = await User.findAll();
-    const expenses = await Expense.findAll();
-
-    const UserExpenses = {};
-
-    expenses.forEach((expense) => {
-      if (UserExpenses[expense.UserId]) {
-        UserExpenses[expense.UserId] =
-          UserExpenses[expense.UserId] + expense.expense;
-      } else {
-        UserExpenses[expense.UserId] = expense.expense;
-      }
-      console.log(UserExpenses);
+    const leadboarddetails = await User.findAll({
+      attributes: [
+        "id",
+        "username",
+        [sequelize.fn("sum", sequelize.col("expenses.expense")), "total_cost"],
+      ],
+      include: [
+        {
+          model: Expense,
+          attributes: [],
+        },
+      ],
+      group: ["user.id"],
+      order: [["total_cost", "DESC"]],
     });
-
-    const leadboarddetails = [];
-    users.forEach((user) => {
-      leadboarddetails.push({
-        name: user.username,
-        total_cost: UserExpenses[user.id],
-      });
-    });
-    leadboarddetails.sort((a, b) => b.total_cost - a.total_cost);
     res.status(200).send(leadboarddetails);
   } catch (err) {
     console.log(err);
